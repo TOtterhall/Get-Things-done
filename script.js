b935d53558b96d183ea8e76ff6aa8dabe4d6357b
Therese Otterhäll <tessan.otterhall@gmail.com>
2022-10-23 16:22:53 +0200

    add part two to practice

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const todoList = document.querySelector(".todos ul");
 const todoInput = document.querySelector(".form input");
 const btn = document.querySelector(".form button");
 
-const todos = ["Hund", "Min familj", "Illustrera", "Roadtrips med husbileen", "Surfa"];
 
 btn.addEventListener("click", addTodos);
 
@@ -11,30 +10,45 @@ renderTodos ();
 function renderTodos () {
     todoList.innerHtml= "";
 
-    for (const todo of todos) {
-        const li = document.createElement("li");
+    const todos = JSON.parse(localStorage.getItem("todos"));
 
-        const liText = document.createTextNode(todo);
-        li.appendChild(liText);
+    if (todos) {
 
-        const icon = document.createElement("i");
-        icon.classList.add("fa-solid", "fa-check");
-        li.appendChild(icon);
-        
-        todoList.appendChild(li);
-        
-        li.addEventListener("click", removeTodo);
+        for (const todo of todos) {
+            const li = document.createElement("li");
+
+            const liText = document.createTextNode(todo);
+            li.appendChild(liText);
+
+            const icon = document.createElement("i");
+            icon.classList.add("fa-solid", "fa-check");
+            li.appendChild(icon);
+            
+            todoList.appendChild(li);
+            
+            li.addEventListener("click", removeTodo);
+        }
     }
 }
 
 function addTodos() {
-   todos.push(todoInput.value)
-   todoInput.value = "";
-   renderTodos();
+
+    if (!localStorage.getItem("todos")) {
+        localStorage.setItem("todos", JSON.stringify([todoInput.value]));
+    } else {
+        const todos = JSON.parse(localStorage.getItem("todos"));
+        todos.push(todoInput.value);
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }
+    todoInput.value= "";
+    renderTodos();
 }
 
 function removeTodo(event) {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    
     const index = todos.indexOf(event.target.innerText);
     todos.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
     renderTodos();
 }
